0dfce84bd4dbbea9941aea6cd530f20eabcf2f0a
trincellshelvin <trincellshelvin@gmail.com>
2024-11-06 22:18:02 -0600

    completed week-4 test folder for book and pet js files with get and set

--- week-4/book-get-set.js
+++ week-4/book-get-set.js
@@ -13,7 +13,47 @@
 // }
 
 class Book {
+    constructor(title, author, publicationYear) {
+        this._title = title;
+        this._author = author;
+        this._publicationYear = publicationYear;
+    }
 
+    get title() {
+        return this._title;
+    }
+
+    set title(value) {
+        if (value.length > 0) {
+            this._title = value;
+        } else {
+            throw new Error('Title cannot be empty.');
+        }
+    }
+
+    get author() {
+        return this._author;
+    }
+
+    set author(value) {
+        if (value.length > 0) {
+            this._author = value;
+        } else {
+            throw new Error('Author cannot be empty.');
+        }
+    }
+
+    get publicationYear() {
+        return this._publicationYear;
+    }
+
+    set publicationYear(value) {
+        if (Number.isInteger(value) && value > 0) {
+            this._publicationYear = value;
+        } else {
+            throw new Error('Publication year must be a positive integer.');
+        }
+    }
 }
 
 export default Book;
--- week-4/pet-get-set.js
+++ week-4/pet-get-set.js
@@ -13,7 +13,64 @@
 //    // Bonus: automatically convert human ages to pet ages when setting.
 // }
 class Pet {
+    constructor(name, type, age) {
+        this._name = name;
+        this._type = type;
+        this._age = this._convertHumanToPetAge(age);
+    }
 
+    // Convert human age to pet age based on type
+    _convertHumanToPetAge(humanAge) {
+        const conversionRates = {
+            dog: 7, // 1 human year is approximately 7 dog years
+            cat: 5, // 1 human year is approximately 5 cat years
+            // Add more types and their conversion rates if needed
+        };
+
+        return conversionRates[this._type] ? humanAge * conversionRates[this._type] : humanAge;
+    }
+
+    // Getter for name
+    get name() {
+        return this._name;
+    }
+
+    // Setter for name with validation
+    set name(value) {
+        if (value.length > 0) {
+            this._name = value;
+        } else {
+            throw new Error('Name cannot be empty.');
+        }
+    }
+
+    // Getter for type
+    get type() {
+        return this._type;
+    }
+
+    // Setter for type with validation
+    set type(value) {
+        if (value.length > 0) {
+            this._type = value;
+        } else {
+            throw new Error('Type cannot be empty.');
+        }
+    }
+
+    // Getter for age
+    get age() {
+        return this._age;
+    }
+
+    // Setter for age with validation
+    set age(value) {
+        if (Number.isInteger(value) && value >= 0) {
+            this._age = this._convertHumanToPetAge(value);
+        } else {
+            throw new Error('Age must be a non-negative integer.');
+        }
+    }
 }
 
-export default Pet
+export default Pet;
