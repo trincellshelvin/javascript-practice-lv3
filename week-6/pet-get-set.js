18ecf94418f0c6c012e1f2da6b1c8ce8f16a4f65
trincellshelvin <trincellshelvin@gmail.com>
2024-11-06 22:58:51 -0600

    completed week-6 for book and pet js files

--- week-6/pet-get-set.js
+++ week-6/pet-get-set.js
@@ -13,7 +13,56 @@
 //    // Bonus: automatically convert human ages to pet ages when setting.
 // }
 class Pet {
+    constructor(name, type, age) {
+        this._name = name;
+        this._type = type;
+        this._age = this._convertHumanToPetAge(age);
+    }
+    
+    _convertHumanToPetAge(humanAge) {
+        const conversionRates = {
+            dog: 7, 
+            cat: 5, 
+        };
 
+        return conversionRates[this._type] ? humanAge * conversionRates[this._type] : humanAge;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        if (value.length > 0) {
+            this._name = value;
+        } else {
+            throw new Error('Name cannot be empty.');
+        }
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    set type(value) {
+        if (value.length > 0) {
+            this._type = value;
+        } else {
+            throw new Error('Type cannot be empty.');
+        }
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        if (Number.isInteger(value) && value >= 0) {
+            this._age = this._convertHumanToPetAge(value);
+        } else {
+            throw new Error('Age must be a non-negative integer.');
+        }
+    }
 }
 
-export default Pet
+export default Pet;
